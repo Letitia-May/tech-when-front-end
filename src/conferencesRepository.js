67116c49e81ad4b0440bbc3e55d1272e4c58b009
Chloe Kuypers <ckuypers@thoughtworks.com>
2018-07-09 18:09:23 +1000

    Fetch conference data from endpoint

--- src/conferencesRepository.js
+++ src/conferencesRepository.js
@@ -1,27 +1,6 @@
 export const findAll = () => {
-	return Promise.resolve({
-		data: [
-			{
-				"name": "Code1",
-				"displayStartDate": "19 Jul 2018",
-				"displayEndDate": "10 Jul 2018",
-				"city": "Melbourne",
-				"website": "www.code1.com.au"
-			},
-			{
-				"name": "Code2",
-				"displayStartDate": "9 Oct 2018",
-				"displayEndDate": "9 Oct 2018",
-				"city": "Melbourne",
-				"website": "www.code2.com.au"
-			},
-			{
-				"name": "Code3",
-				"displayStartDate": "1 Nov 2018",
-				"displayEndDate": "3 Nov 2018",
-				"city": "Melbourne",
-				"website": "www.code3.com.au"
-			}
-		]
-	});
+	return fetch('REPLACE_WITH_URL')
+		.then(response => response.json())
+		.then(body => ({data: body}))
+		.catch(error => ({data: []}));
 };
