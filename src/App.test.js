a8629d700ec8e4dfc5ae3103f45e261ae46914f6
Pam Rucinque <pamela.rucinque@gmail.com>
2018-09-26 18:11:48 +1000

    [Pam, Chloe] Stub out the conferences list request in App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,44 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from './App';
 import ConferencesList from './conferences/ConferencesList';
+import * as conferencesRepository from './conferencesRepository';
+
+import sinon from 'sinon';
 
 describe('App', () => {
+
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    const stubbedConferences = Promise.resolve({
+      json: () => {
+        return [
+          {
+            name: "Code1",
+            displayStartDate: "19 Jul 2018",
+            displayEndDate: "10 Jul 2018",
+            city: "Melbourne",
+            website: "www.code1.com.au"
+          },
+          {
+            name: "Code2",
+            displayStartDate: "1 Nov 2018",
+            displayEndDate: "5 Nov 2018",
+            city: "Melbourne",
+            website: "www.code2.com.au"
+          }
+        ];
+      }
+    })
+
+    sandbox.stub(conferencesRepository, "findAll").returns(stubbedConferences);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('renders without crashing', () => {
     shallow(<App />);
   });
